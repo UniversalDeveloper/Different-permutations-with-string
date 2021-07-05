d9119cbc08fefa9f4fa1fb4c61b1a0d50f58bceb
lopataA <lopata.ad@gmail.com>
2021-07-05 17:55:05 +0300

    Optimized test method.

--- WordComb/WordComb/Program.cs
+++ WordComb/WordComb/Program.cs
@@ -25,60 +25,28 @@ namespace WordComb
         }
         public static void swap(ref char a, ref char b)
         {
-            char temp = a;
+            var temp = a;
             a = b;
             b = temp;
         }
         public static void wordPermutTest(string str)
         {
-            switch (str)
+            var expLenTree = new string[] { "213", "231", "321", "312", "132", "123" };
+            var expLenFour = new string[] { "2134", "2314", "2341", "3241", "3421", "3412", "4312", "4132", "4123", "1423", "1243", "1234" };
+            var expLenFive = new string[] { "21345", "23145", "23415", "23451", "32451", "34251", "34521", "34512", "43512", "45312", "45132", "45123", "54123", "51423", "51243", "51234", "15234", "12534", "12354", "12345" };
+            var actualRes = permutOfSymbolInString(str);
+            if (expLenTree.SequenceEqual(actualRes) || expLenFour.SequenceEqual(actualRes) || expLenFive.SequenceEqual(actualRes))
             {
-                case "123":
-                    string[] expLenTree = new string[] { "213", "231", "321", "312", "132", "123" };
-                    string[] actualLenTree = Program.permutOfSymbolInString(str);
-                    if (expLenTree.SequenceEqual(actualLenTree))
-                    {
-                        Console.WriteLine("True for string 123: shows 6 variants of permutations.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("False: does not worked for string 123");
-                    }
-                    break;
-
-                case "1234":
-                    string[] expLenFour = new string[] { "2134", "2314", "2341", "3241", "3421", "3412", "4312", "4132", "4123", "1423", "1243", "1234" };
-                    string[] actualLenFour = Program.permutOfSymbolInString(str);
-                    if (expLenFour.SequenceEqual(actualLenFour))
-                    {
-                        Console.WriteLine("True for string 1234: shows 12 variants of permutations.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("False: does not worked for string 1234");
-                    }
-
-                    break;
-
-                case "12345":
-                    string[] expLenFive = new string[] { "21345", "23145", "23415", "23451", "32451", "34251", "34521", "34512", "43512", "45312", "45132", "45123", "54123", "51423", "51243", "51234", "15234", "12534", "12354", "12345" };
-                    string[] actualLenFive = Program.permutOfSymbolInString(str);
-                    if (expLenFive.SequenceEqual(actualLenFive))
-                    {
-                        Console.WriteLine("True for string 12345: shows 20 variants of permutations.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("False: does not worked for string 12345");
-                    }
-
-                    break;
+                Console.WriteLine("True. Prmutations are correct.");
             }
-
+            else
+            {
+                Console.WriteLine("False: Prmutations are not correct.");
+            }
         }
         public static void showPermutations(string str)
         {
-            var resaltArr = Program.permutOfSymbolInString(str);
+            var resaltArr = permutOfSymbolInString(str);
 
             foreach (string s in resaltArr)
             {
@@ -88,14 +56,9 @@ namespace WordComb
         }
         static void Main(string[] args)
         {
-            showPermutations("123");
             wordPermutTest("123");
-            showPermutations("1234");
             wordPermutTest("1234");
-            showPermutations("12345");
             wordPermutTest("12345");
-
-
         }
     }
 }
